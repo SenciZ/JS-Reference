876dff5e5a53d4428059964f721aed1c16ef3dae
SenciZ <zenkovic88@gmail.com>
2021-05-30 15:46:12 -0500

    Add file

---
+++ Reference.js
@@ -0,0 +1,42 @@
+// Object Literal 
+const myObject = {
+    property: 'Value!',
+    otherProperty: 77,
+    "obnoxious property": function() {
+      // do stuff!
+   }
+  }
+
+
+  // Object Constructor
+  function Player(name, marker) {
+    this.name = name
+    this.marker = marker
+    this.sayName = function() {
+      console.log(name)
+    }
+  }
+  
+  const player1 = new Player('steve', 'X')
+
+  // Factory Function
+  const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+  
+  const jeff = personFactory('jeff', 27);
+
+  //Module
+  const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
